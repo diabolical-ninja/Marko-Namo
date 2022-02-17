5f6cfc5e100844ea6b65d0b7d833ee35c85677a7
yassin-eltahir <15998949+diabolical-ninja@users.noreply.github.com>
2022-02-17 13:50:48 +1100

    Only run linting

---
+++ noxfile.py
@@ -0,0 +1,40 @@
+"""Configuration of nox for testing & code validation."""
+
+import nox
+from nox.sessions import Session
+
+PYTHON_VERSION = "3.8"
+
+
+@nox.session(python=PYTHON_VERSION)
+def lint(session: Session) -> None:
+    """Runs linting checks."""
+    session.install(
+        "flake8",
+        "flake8-docstrings",
+        "flake8-import-order",
+        "flake8-black",
+        "darglint",
+        "flake8-annotations",
+        "flake8-quotes",
+        "flake8-requirements",
+        "pep8-naming",
+    )
+    session.run("flake8", "src/")
+    session.run("flake8", "generate_names.py")
+
+
+# @nox.session(python=PYTHON_VERSION)
+# def tests(session: Session) -> None:
+#     """Runs unit testing & generates coverage report."""
+#     session.install("pytest", "pytest-cov", "pytest-xdist")
+#     session.run("pip", "install", "-r", "requirements.txt")
+#     session.run("pytest", "--cov")
+
+
+# @nox.session(python=PYTHON_VERSION)
+# def coverage(session: Session) -> None:
+#     """Upload coverage data."""
+#     session.install("coverage", "codecov")
+#     session.run("coverage", "xml", "--fail-under=0")
+#     session.run("codecov")
